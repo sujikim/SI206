2043791622d2805dc764ee44484e9bcf95eb4110
sujikim <sujikim@umich.edu>
2016-11-07 01:31:06 -0500

    madlib

---
+++ madlibhw3.py
@@ -0,0 +1,32 @@
+from nltk.book import text2
+
+
+
+# Requirements:
+# 1) Only use the first 150 tokens
+# 2) Pick 5 parts of speech to prompt for, including nouns
+# 3) Replace nouns 15% of the time, everything else 10%
+
+# Deliverables:
+# 1) Print the orginal text (150 tokens)
+# 1) Print the new text
+print("START*******")
+
+
+print("\n\nEND*******")
+from nltk.book import *
+from nltk import bigrams
+print(text1)
+print(text2)
+
+# print ("\n\n")
+# print ("Length of ",text1,"is",len(text1))
+# print ("Length of ", text2,"is",len(text2))
+# print ("Unique tokens in", text1, " are: ", len(set(text1)))
+# print ("Unique tokens in", text2, " are: ", len(set(text2)))
+
+# print ("\n\nList of unique words")
+
+# print (sorted(set(text2))[:15])
+# print (sorted(set(text2), reverse = True) [:15])
+
